a3120910bfd442a30da68160ea4ec4510c2888ae
Gaupy <https://github.com/gaupy>
2014-05-21 15:28:45 +0200

    bfs algo.

--- schedule.ml
+++ schedule.ml
@@ -4,7 +4,7 @@ open Tools
 
 
 
-let dfs_v1 dag =
+let dfs_compare dag compare_fun =
 	let ntasks = Array.length dag.tabTask in
 	let result = {order = Array.make ntasks (-1,false); sched = Array.make ntasks (-1,false)} in
 	let current = ref 0 in (* The position of the current task in the linearization*)
@@ -18,15 +18,16 @@ let dfs_v1 dag =
 			result.order.(!current) <- (taskId , false);
 			incr current;
 			
-			(* Finally, we execute the dfs order. Note that the next tweak will sort in a specific manner dag.tabChildren.(taskId)*)
-			List.iter auxdfs_v1 dag.tabChildren.(taskId)
+			(* compare_fun chooses the function for dfs.*)
+			let childrenSorted = List.fast_sort (compare_fun) dag.tabChildren.(taskId) in
+			List.iter auxdfs_v1 childrenSorted
 		end
 	in
 	List.iter auxdfs_v1 dag.sources;
 	if !current <> ntasks then (Printf.printf "Not everyone has been scheduled: %d." !current; failwith "\n") ; 
 	result
 
-let dfs_v2 dag =
+let dfs dag =
 	let ntasks = Array.length dag.tabTask in
 	let result = {order = Array.make ntasks (-1,false); sched = Array.make ntasks (-1,false)} in
 	let current = ref 0 in (* The position of the current task in the linearization*)
@@ -50,3 +51,23 @@ let dfs_v2 dag =
 	result
 
 
+let bfs dag =
+	let ntasks = Array.length dag.tabTask in
+	let result = {order = Array.make ntasks (-1,false); sched = Array.make ntasks (-1,false)} in
+	let queueTBS = Queue.create () in
+	List.iter (fun x-> Queue.add x queueTBS) dag.sources;
+	let current = ref 0 in (* The position of the current task in the linearization*)
+	while !current < ntasks do
+		let taskId = Queue.pop queueTBS in
+		if  snd result.sched.(taskId) then failwith "not a DAG?";
+		(* We verify whether this element can be scheduled. If it cannot we do nothing.*)
+		if (List.for_all (fun x -> snd result.sched.(x)) dag.tabParents.(taskId)) then 
+		begin
+			result.sched.(taskId) <- (!current,true);
+			result.order.(!current) <- (taskId,true);
+			List.iter (fun x-> Queue.add x queueTBS) dag.tabChildren.(taskId);
+			incr current;
+		end
+	done;
+	if not (Queue.is_empty queueTBS) then failwith "not a DAG?";
+	result
